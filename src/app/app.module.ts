d45d7b8fe451ba17167ba2e8ba8be1848a87636d
joaquinS25 <112294606+joaquinS25@users.noreply.github.com>
2023-02-20 17:14:11 -0500

    PROYECTO FINAL

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { appReducer } from './store/app.reducer';
+import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
   declarations: [
@@ -42,6 +43,8 @@ import { appReducer } from './store/app.reducer';
     HttpClientModule,    
     StoreModule.forRoot(appReducer, {}),
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
+    StoreModule.forRoot({}, {}),
+    EffectsModule.forRoot([]),
   ],
   providers: [],
   bootstrap: [AppComponent]
