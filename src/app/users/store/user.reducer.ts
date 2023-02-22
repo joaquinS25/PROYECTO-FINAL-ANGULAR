c5e142ba0f7b7a52c85ccc17c997da47140c556d
joaquinS25 <112294606+joaquinS25@users.noreply.github.com>
2023-02-21 23:03:54 -0500

    PROYECTO FINAL

--- src/app/users/store/user.reducer.ts
+++ src/app/users/store/user.reducer.ts
@@ -1,14 +1,15 @@
 import { Action, createReducer, on } from '@ngrx/store';
+import { User } from 'src/app/models/user.model';
 import * as UserActions from './user.actions';
 
 export const userFeatureKey = 'user';
 
 export interface State {
-
+    data: User[]
 }
 
 export const initialState: State = {
-
+    data: [],
 };
 
 export const reducer = createReducer(
